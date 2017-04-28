f19a93b3cd67e34dd966d37af1e5d9cc6c52eb73
Jay Herman <hytek1999@gmail.com>
2017-04-28 15:20:15 -0500

    Initial level editor coding.

---
+++ source/ultimate-wizard/Assets/Scripts/Controllers/CursorController.cs
@@ -0,0 +1,53 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using System;
+
+public class CursorController : MonoBehaviour {
+    public float cursorSpeed = 1.0f; 
+
+    private Vector3 targetPosition;
+    private bool isMoving;
+    private Animator animator;
+
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+    void OnEnable()
+    {
+        this.AddObserver(OnMoveEvent, InputController.MoveNotification);
+        
+    }
+
+    void OnDisable()
+    {
+        this.RemoveObserver(OnMoveEvent, InputController.MoveNotification);
+    }
+
+    private void OnMoveEvent(object sender, object e)
+    {
+        Vector3 moveTo = (Vector3)e;
+        if (!isMoving)
+        {
+            targetPosition = transform.position + moveTo;
+            StartCoroutine(MoveCursor());
+        }
+    }
+
+    private IEnumerator MoveCursor()
+    {
+        isMoving = true;
+        animator.SetBool("isMoving", true);
+        Vector3 currentPosition = transform.position;
+        while (Mathf.Abs(Vector3.Distance(transform.position, targetPosition)) > 0.1f)
+        {
+            transform.position = Vector3.Lerp(currentPosition, targetPosition, 1.0f / (cursorSpeed * Time.deltaTime));
+            yield return null;
+        }
+        transform.position = targetPosition;
+        animator.SetBool("isMoving", false);
+        isMoving = false;
+    }
+}
---
+++ source/ultimate-wizard/Assets/Scripts/Controllers/InputController.cs
@@ -0,0 +1,70 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+
+public class InputController : MonoBehaviour {
+
+    class Repeater
+    {
+        const float threshold = 0.5f;
+        const float rate = 0.25f;
+        float _next;
+        bool _hold;
+        string _axis; 
+
+        public Repeater (string axisName)
+        {
+            _axis = axisName;
+        }
+
+        public int Update()
+        {
+            int retValue = 0;
+            int value = Mathf.RoundToInt(Input.GetAxisRaw(_axis));
+
+            if (value != 0)
+            {
+                if (Time.time > _next)
+                {
+                    retValue = value;
+                    _next = Time.time + (_hold ? rate : threshold);
+                    _hold = true;
+                }
+            }
+            else
+            {
+                _hold = false;
+                _next = 0;
+            }
+            return retValue;
+        }
+    }
+
+    Repeater _hor = new Repeater("Horizontal");
+    Repeater _ver = new Repeater("Vertical");
+
+    #region Notifications
+    public const string MoveNotification = "InputController.MoveNotification";
+    public const string FireNotification = "InputController.FireNotification";
+    #endregion
+
+    string[] _buttons = new string[] { "Fire1", "Fire2", "Fire3" };
+	
+	// Update is called once per frame
+	void Update () {
+        int x = _hor.Update();
+        int y = _ver.Update(); 
+        if (x != 0 || y != 0)
+        {
+            this.PostNotification(MoveNotification, new Vector3(x, y, 0));
+        }
+
+        for (int i = 0; i < 3; ++i)
+        {
+            if (Input.GetButtonUp(_buttons[i]))
+            {
+                this.PostNotification(FireNotification, i);
+            }
+        }
+	}
+}
